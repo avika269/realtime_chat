b639b8b29dc456c7f40164f24fbb673f708591d5
Avika Agarwal <agkaviz.097@gmail.com>
2026-09-26 17:52:30 +0530

    commit

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,5 @@
 import Message from "../models/Message.js";
-import Conversation from "../models/Conversation.js";
+import Conversation from "../models/conversation.js";
 
 export const sendMessage = async (
     req,
